592cb09b251a84d58ca1ff450de99c509ac5addb
Dean Edis <dean.edis@gmail.com>
2026-02-08 19:11:04 +0000

    Other: Remove the big-endian code.
    Not needed. Yet.

--- Bus.cs
+++ Bus.cs
@@ -110,27 +110,6 @@ public sealed class Bus
     }
 
     /// <summary>
-    /// Reads a 24-bit value in big-endian order.
-    /// </summary>
-    public uint Read24BigEndian(uint address)
-    {
-        var b0 = Read8(address);
-        var b1 = Read8(address + 1);
-        var b2 = Read8(address + 2);
-        return (uint)((b0 << 16) | (b1 << 8) | b2);
-    }
-
-    /// <summary>
-    /// Writes a 24-bit value in big-endian order.
-    /// </summary>
-    public void Write24BigEndian(uint address, uint value)
-    {
-        Write8(address, (byte)((value >> 16) & 0xFF));
-        Write8(address + 1, (byte)((value >> 8) & 0xFF));
-        Write8(address + 2, (byte)(value & 0xFF));
-    }
-
-    /// <summary>
     /// Reads a 32-bit value in big-endian order.
     /// </summary>
     public uint Read32BigEndian(uint address)
@@ -142,17 +121,6 @@ public sealed class Bus
         return (uint)((b0 << 24) | (b1 << 16) | (b2 << 8) | b3);
     }
 
-    /// <summary>
-    /// Writes a 32-bit value in big-endian order.
-    /// </summary>
-    public void Write32BigEndian(uint address, uint value)
-    {
-        Write8(address, (byte)((value >> 24) & 0xFF));
-        Write8(address + 1, (byte)((value >> 16) & 0xFF));
-        Write8(address + 2, (byte)((value >> 8) & 0xFF));
-        Write8(address + 3, (byte)(value & 0xFF));
-    }
-    
     public byte ReadPort(ushort portAddress) =>
         m_portDevice?.Read8(portAddress) ?? 0xFF;
 
